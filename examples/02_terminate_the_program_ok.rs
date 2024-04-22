6c8f46e6ef2ef9d5cf8ca18bb834577eba10d183
Mathias <email@example.com>
2024-04-22 08:55:36 +0200

    -> Add AFTER housekeeping => examples//02_terminate_the_program_ok.rs

--- examples/02_terminate_the_program_ok.rs
+++ examples/02_terminate_the_program_ok.rs
@@ -1,9 +1,8 @@
-
 use std::fs;
 
 fn main() {
-  let content = fs::read_to_string("./Cargo.toml").expect("Can't read Cargo.toml");
-  println!("{}", content)
+    let content = fs::read_to_string("./Cargo.toml").expect("Can't read Cargo.toml");
+    println!("{}", content)
 }
 
 /*
