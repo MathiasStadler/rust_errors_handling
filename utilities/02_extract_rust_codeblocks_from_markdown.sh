da7ad4d8b19868f989248a2da40bc4b4ca8e885f
Mathias <email@example.com>
2024-04-28 11:45:41 +0200

    save

--- utilities/02_extract_rust_codeblocks_from_markdown.sh
+++ utilities/02_extract_rust_codeblocks_from_markdown.sh
@@ -197,7 +197,7 @@ echo "ReturnCode => \$?"
 */
 EoF
 #!/usr/bin/env bash
-export EXAMPLE_SCRIPT_FILE="04_ok_bubble_up_the_error.rs"
+export EXAMPLE_SCRIPT_FILE="04_ok_bubble_up_error.rs"
 export EXAMPLE_SCRIPT_DIR="examples/"
 cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
 // FORM HERE
@@ -255,6 +255,446 @@ echo "ReturnCode => \$?"
 */
 EoF
 #!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="04_err_bubble_up_error.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+// FORM HERE
+// https://www.sheshbabu.com/posts/rust-error-handling/
+
+use std::collections::HashMap;
+
+fn get_current_date() -> Result<String, reqwest::Error> {
+  let url = "https://postman-echo.com/time/object";
+  let result = reqwest::blocking::get(url);
+
+  let response = match result {
+    Ok(res) => res,
+    Err(err) => return Err(err),
+  };
+
+  let body = response.json::<HashMap<String, i32>>();
+
+  let json = match body {
+    Ok(json) => json,
+    Err(err) => return Err(err),
+  };
+
+  // Expected Err =>  No entry found for key
+  let date = json["ERROR_HERE"].to_string();
+
+  Ok(date)
+}  
+
+pub fn main() {
+  match get_current_date() {
+    Ok(date) => println!("We've time travelled to {}!!", date),
+    Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+  }
+}
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_ok_bubble_up_multiple_errors_does_not_work.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+// FROM HERE
+// https://github.com/sheshbabu/rust-error-handling-examples/blob/master/05-bubble-up-multiple-errors/src/main.rs
+
+
+// WARNING
+// The follow code won’t compile as parse_from_str returns a chrono::format::ParseError error and NOT reqwest::Error
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+    }
+}
+
+fn get_current_date() -> Result<String, reqwest::Error> {
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/object";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    // Error here = wrong Result 
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_ok_with_box_bubble_up_multiple_errors_.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/object";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_err_with_box_bubble_up_multiple_errors_first_error.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // First error with change
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/objectzzzz";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+    
+    // Second error WITHOUT change
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_err_with_box_bubble_up_multiple_errors_second_error.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // First error  WITHOUT change
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/object";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+    
+    // Second error with change
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}z", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_ok_match_boxed_errors_in_fn_main.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+//FROM HERE
+//https://github.com/sheshbabu/rust-error-handling-examples/blob/master/06-match-boxed-errors/src/main.rs
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => {
+            eprintln!("Oh noes, we don't know which era we're in! :(");
+            if let Some(err) = e.downcast_ref::<reqwest::Error>() {
+                eprintln!("Request Error: {}", err)
+            } else if let Some(err) = e.downcast_ref::<chrono::format::ParseError>() {
+                eprintln!("Parse Error: {}", err)
+            }
+        }
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/object";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_err_request_match_boxed_errors_in_fn_main.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+//FROM HERE
+//https://github.com/sheshbabu/rust-error-handling-examples/blob/master/06-match-boxed-errors/src/main.rs
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => {
+            eprintln!("Oh noes, we don't know which era we're in! :(");
+            if let Some(err) = e.downcast_ref::<reqwest::Error>() {
+                eprintln!("Request Error: {}", err)
+            } else if let Some(err) = e.downcast_ref::<chrono::format::ParseError>() {
+                eprintln!("Parse Error: {}", err)
+            }
+        }
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // /w err reqwest::Error - wrong url
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/objectzzzz";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
+export EXAMPLE_SCRIPT_FILE="05_err_parse_match_boxed_errors_in_fn_main.rs"
+export EXAMPLE_SCRIPT_DIR="examples/"
+cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
+//FROM HERE
+//https://github.com/sheshbabu/rust-error-handling-examples/blob/master/06-match-boxed-errors/src/main.rs
+use chrono::NaiveDate;
+use std::collections::HashMap;
+
+fn main() {
+    match get_current_date() {
+        Ok(date) => println!("We've time travelled to {}!!", date),
+        Err(e) => {
+            eprintln!("Oh noes, we don't know which era we're in! :(");
+            if let Some(err) = e.downcast_ref::<reqwest::Error>() {
+                eprintln!("Request Error: {}", err)
+            } else if let Some(err) = e.downcast_ref::<chrono::format::ParseError>() {
+                eprintln!("Parse Error: {}", err)
+            }
+        }
+    }
+}
+
+fn get_current_date() -> Result<String, Box<dyn std::error::Error>> {
+    // Try changing the url to "https://postman-echo.com/time/objectzzzz"
+    let url = "https://postman-echo.com/time/object";
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
+
+    // /w err chrono::format::ParseError - wrong format pattern
+    // Try changing the format to "{}-{}-{}z"
+    let formatted_date = format!("{}-{}-{}z", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
+
+    Ok(date)
+}
+
+
+
+/*
+export FILE_NAME=$EXAMPLE_SCRIPT_FILE
+export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
+git add \$FILE_DIR_NAME/\$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+# git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
+git push
+cargo run --example "\$(echo \$FILE_NAME | cut -d . -f 1)"
+echo "ReturnCode => \$?"
+*/
+EoF
+#!/usr/bin/env bash
 export EXAMPLE_SCRIPT_FILE="99_template.rs"
 export EXAMPLE_SCRIPT_DIR="examples/"
 cat << EoF > ./$EXAMPLE_SCRIPT_DIR/$EXAMPLE_SCRIPT_FILE
@@ -268,7 +708,7 @@ export FILE_NAME=$EXAMPLE_SCRIPT_FILE
 export FILE_DIR_NAME=$EXAMPLE_SCRIPT_DIR
 git add \$FILE_DIR_NAME/\$FILE_NAME
 git commit --all --message="-> Add BEFORE housekeeping => \$FILE_DIR_NAME/\$FILE_NAME"
-git push
+# git push
 # cargo install --list
 # cargo update --workspace
 cargo clippy --fix
